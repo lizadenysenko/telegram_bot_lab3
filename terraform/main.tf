acc2e572d67646e689fe1bd76ce964aee988887b
Liza Denysenko <106141102+lizadenysenko@users.noreply.github.com>
2025-04-16 12:03:43 +0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "eu-central-1"
+}
 
+resource "aws_instance" "telegram_bot" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 22.04
+  instance_type = "t2.micro"
+  key_name      = "your-key"
+  tags = {
+    Name = "TelegramBot"
+  }
+}
